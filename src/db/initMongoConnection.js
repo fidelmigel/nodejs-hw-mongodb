fc69d3d4302211943790e555c25ea8c1a3c472ce
Mykhailo Kalinichenko <99479355+fidelmigel@users.noreply.github.com>
2025-03-04 21:02:50 +0100

    Mongo connection successfully established!

---
+++ src/db/initMongoConnection.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+import { getEnvVar } from '../utils/getEnvVar.js';
+
+export const initMongoConnection = async () => {
+  try {
+    const user = getEnvVar('MONGODB_USER');
+    const pwd = encodeURIComponent(getEnvVar('MONGODB_PASSWORD'));
+    const url = getEnvVar('MONGODB_URL');
+    const db = getEnvVar('MONGODB_DB');
+
+    const connectionString = `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`;
+
+    await mongoose.connect(connectionString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log('Mongo connection successfully established!');
+  } catch (e) {
+    console.error('Error while setting up mongo connection', e);
+    throw e;
+  }
+};
